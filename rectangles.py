145da3fc0068ec9650f3307663fdda3587104b8c
lexij07 <145355812+lexij07@users.noreply.github.com>
2023-10-12 09:11:49 -0400

    rectangle

--- rectangles.py
+++ rectangles.py
@@ -9,7 +9,7 @@ pygame.init()
 
 # Create and open a pygame screen with the given size
 width = 640
-height = 360
+height = 220
 
 screen = pygame.display.set_mode((width, height))
 
@@ -17,11 +17,23 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("My Game")
 
 ### PUT YOUR GAME CODE HERE
+screen.fill("lightgray")
 
-
+box = pygame.Rect(50, 20, 120, 100)
+pygame.draw.rect(screen, "red", box)
+box = pygame.Rect(100, 60, 120, 100)
+pygame.draw.rect(screen, "green", box)
+box = pygame.Rect(150, 100, 120, 100)
+pygame.draw.rect(screen, "blue", box)
+box = pygame.Rect(350, 20, 120, 100)
+pygame.draw.rect(screen, "red", box, 1)
+box = pygame.Rect(400, 60, 120, 100)
+pygame.draw.rect(screen, "green", box, 4)
+box = pygame.Rect(450, 100, 120, 100)
+pygame.draw.rect(screen, "blue", box, 8)
 
 # Uncomment this line to show a grid
-# draw_grid()
+draw_grid()
 
 # Flip the changes to the screen to the computer display
 pygame.display.flip()
